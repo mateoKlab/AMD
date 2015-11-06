212797519daf36fb135ff3fff164a904365721b8
Matthew Trinidad <m.trinidad@klab.com>
2015-11-06 15:24:38 +0800

    Created PlayerData and StageData files.

---
+++ TournamentManager/Assets/Resources/Scripts/PlayerData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerData {
+
+	public List<FighterData> fightersOwned = new List<FighterData> ();
+
+}
