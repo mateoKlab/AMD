09b716b4acf5df60a920f8ae4e6b18ee218becfd
Matthew Trinidad <m.trinidad@klab.com>
2015-11-09 16:08:38 +0800

    Added Win and Player Killed event.

--- TournamentManager/Assets/Bingo/Data/EventTags.cs
+++ TournamentManager/Assets/Bingo/Data/EventTags.cs
@@ -1,5 +1,7 @@
 // ----- AUTO GENERATED CODE ----- //
 public enum EventTags
 {
-    DEFAULT
+    DEFAULT,
+	FIGHTER_KILLED, 	// args[0]: typeof:FighterData  desc: The fighter killed.
+	STAGE_WIN 			// args[0]: typeof:StageData    desc: The stage beaten.
 }
