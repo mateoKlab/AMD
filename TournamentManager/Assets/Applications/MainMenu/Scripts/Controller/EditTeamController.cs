a36b63d07234bd143c60d5897d1d7ef2f7e31c32
Dave Invierno <d.invierno@klab.com>
2015-11-09 08:48:58 +0800

    Initial edit team screen/popup

---
+++ TournamentManager/Assets/Applications/MainMenu/Scripts/Controller/EditTeamController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Bingo;
+
+public class EditTeamController : Controller
+{
+	private Transform activeTeamPanel;
+	private Transform teamPanel;
+
+	public override void Awake ()
+	{
+		base.Awake ();
+
+		activeTeamPanel = transform.FindChild("ActiveTeamPanel");
+		teamPanel = transform.FindChild("TeamPanel");
+	}
+
+	public void ReturnTroopFromSlotToTeamPanel(GameObject troop) 
+	{
+		troop.transform.SetParent(teamPanel);
+		troop.transform.SetAsLastSibling();
+	}
+
+	public bool CheckIfTroopChildOfTeamPanel(GameObject troop) 
+	{
+		return teamPanel.IsChildOf(teamPanel);
+	}
+
+}
