074ccfba1c0605348afd3b72ab0d9d6830243afa
Matthew Trinidad <m.trinidad@klab.com>
2015-11-12 10:49:07 +0800

    Added documentation for new event parameter

--- TournamentManager/Assets/Bingo/Data/EventTags.cs
+++ TournamentManager/Assets/Bingo/Data/EventTags.cs
@@ -3,5 +3,7 @@ public enum EventTags
 {
     DEFAULT,
 	FIGHTER_KILLED, 	// args[0]: typeof:FighterData  desc: The fighter killed.
+						// args[1]: typeof:FighterData  desc: The attacker.
+
 	STAGE_WIN 			// args[0]: typeof:StageData    desc: The stage beaten.
 }
