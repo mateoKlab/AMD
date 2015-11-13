7452c87daba2b314a3ebe11f90f867189214f7f8
Ardeus Parducho <ardeus@klab.com>
2015-11-13 12:02:23 +0800

    Changed Gacha character sprites

--- TournamentManager/Assets/Bingo/Data/EventTags.cs
+++ TournamentManager/Assets/Bingo/Data/EventTags.cs
@@ -1,6 +1,7 @@
 // ----- AUTO GENERATED CODE ----- //
 public enum EventTags
 {
+	FIGHTER_RECEIVED_DAMAGE,
 	FIGHTER_KILLED, 	// args[0]: typeof:FighterData  desc: The fighter killed.
 						// args[1]: typeof:FighterData  desc: The attacker.
 
