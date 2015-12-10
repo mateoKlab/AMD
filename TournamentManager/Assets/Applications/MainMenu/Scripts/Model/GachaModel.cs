ec574da2bafba828d96027a08ba667033e129901
Ardeus Parducho <ardeus@klab.com>
2015-12-10 11:22:18 +0800

    Gacha: Display sprite based on character's class

--- TournamentManager/Assets/Applications/MainMenu/Scripts/Model/GachaModel.cs
+++ TournamentManager/Assets/Applications/MainMenu/Scripts/Model/GachaModel.cs
@@ -6,6 +6,4 @@ using System.Collections.Generic;
 public class GachaModel : Model
 {
 	public int rollCost = 100;
-	public List<string> namePool = new List<string> {"Alex", "Tyler", "Allie", "Jiro", "Joe", "Jollie", "Oscar", "Jose", "Joey", "Zodiac", "Patrick"};
-	public List<string> classPool = new List<string> {"Warrior", "Knight", "Magician", "Archer", "Thief"};
 }
