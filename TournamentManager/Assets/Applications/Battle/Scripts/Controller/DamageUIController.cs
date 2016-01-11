d2a25c390ad38b03cb841f6f0edeb4a78461c0a8
Ardeus Parducho <ardeus@klab.com>
2016-01-11 14:17:06 +0800

    Damage UI

---
+++ TournamentManager/Assets/Applications/Battle/Scripts/Controller/DamageUIController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Bingo;
+
+public class DamageUIController : Controller <DamageManager, DamageUIModel, DamageUIView>
+{
+	public void ShowDamage(int damageReceived) {
+		view.damageLabel.text = damageReceived.ToString();
+		view.damageAnim.SetTrigger("PopUp");
+	}
+
+	public void AddToDamagePool(){
+		DamageManager.instance.AddToPool(gameObject);
+		gameObject.SetActive(false);
+	}
+}
