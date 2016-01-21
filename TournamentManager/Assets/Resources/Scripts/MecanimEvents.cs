dd35b2e674863a1f37339bf72b919dab26e01517
lawrencemercado <christopher.mercado@klab.com>
2016-01-21 10:36:28 +0800

    add mage prefab

---
+++ TournamentManager/Assets/Resources/Scripts/MecanimEvents.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MecanimEvents : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Fire() {
+		Debug.Log("Fire");
+	}
+}
